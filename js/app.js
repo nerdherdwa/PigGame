a3411521304b759e92d9fca0f2fa7c42aafd8974
nerdherdwa <michael.l.horsfall@gmail.com>
2020-02-20 08:14:57 +0000

    added code challenges

--- js/app.js
+++ js/app.js
@@ -8,35 +8,67 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice, diceDOM, gamePlaying;
+var scores, roundScore, activePlayer, dice, diceDOM, gamePlaying, previousRoll, winScore;
 
 newGame();
 
 
+document.getElementById('win-score').addEventListener('change', function() {
+    document.getElementById('win-score').disabled = true;
+});
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
-        var dice = Math.floor(Math.random() * 6) + 1;
-    
-        diceDOM.style.display = 'block';
-        diceDOM.src = '/imgs/dice-' + dice + '.png';
+        document.getElementById('win-score').disabled = true;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        dice = [dice1,dice2];
+
+        diceDOM1.style.display = 'block';
+        diceDOM1.src = '/imgs/dice-' + dice[0] + '.png';
+
+        diceDOM2.style.display = 'block';
+        diceDOM2.src = '/imgs/dice-' + dice[1] + '.png';
         //3. update the round score if the rolled number was not a one
-        if (dice !== 1) {
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
+        
+        if (dice[0] === 1 || dice[1] === 1) {
+            document.getElementById('message').textContent = "you rolled a one. lose your round score";
             nextPlayer();
+        } else if (dice[0] === 6 || dice[1]===6) {
+            if (dice[0] === dice[1]) {
+                document.getElementById('message').textContent = "you rolled a double 6. lose your score";
+                document.querySelector('#current-' + activePlayer).textContent = 0;
+                document.querySelector('#score-' + activePlayer).textContent = 0;
+                nextPlayer(); 
+            } else if (dice[0] === previousRoll[activePlayer][0] || dice[1] === previousRoll[activePlayer][1] ) {
+                document.getElementById('message').textContent = "you rolled a double 6. lose your score";
+                document.querySelector('#current-' + activePlayer).textContent = 0;
+                document.querySelector('#score-' + activePlayer).textContent = 0;
+                nextPlayer(); 
+            } else {
+                roundScore += dice[0] + dice[1];
+                document.querySelector('#current-' + activePlayer).textContent = roundScore;
+                previousRoll[activePlayer] = dice;
+            }
+        } else {
+            roundScore += dice[0] + dice[1];
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+            previousRoll[activePlayer] = dice;
         }
     }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
+        winScore = document.getElementById("win-score").value;
+        console.log(winScore);
         // add hold score to player score
         scores[activePlayer] += roundScore;
+        previousRoll[activePlayer] = [0,0];
         // update UI 
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
         // check player won game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!!!!';
             diceDOM.style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel' ).classList.remove('active');
@@ -53,25 +85,29 @@ document.querySelector('.btn-new').addEventListener('click', newGame);
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
-        
+    previousRoll = [[0,0],[0,0]];   
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
     
-    diceDOM.style.display = 'none';
+    diceDOM1.style.display = 'none';
+    diceDOM2.style.display = 'none';
 }
 
 function newGame() {
     scores = [0,0];
     activePlayer = 0;
     roundScore = 0;
+    previousRoll = [[0,0],[0,0]];
     gamePlaying = true;
-    diceDOM = document.querySelector('.dice');
+    diceDOM1 = document.querySelector('.dice-1');
+    diceDOM2 = document.querySelector('.dice-2');
 
-    document.querySelector('.dice').style.display = 'none';
-    
+    document.querySelector('.dice-1').style.display = 'none';
+    document.querySelector('.dice-2').style.display = 'none';
+    document.getElementById("win-score").defaultValue = "100";
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
@@ -83,4 +119,5 @@ function newGame() {
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
+    document.getElementById('win-score').disabled = false;
 }
